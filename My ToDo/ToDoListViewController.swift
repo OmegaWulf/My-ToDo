73998cd8bb345c185a97051da1ffd145e94f2204
Nikita Thomas <nikcxz@gmail.com>
2018-09-26 16:35:44 -0400

    Data persisted using UserDefaults method.

--- My ToDo/ToDoListViewController.swift
+++ My ToDo/ToDoListViewController.swift
@@ -12,10 +12,16 @@ class ToDoListViewController: UITableViewController {
 
     var itemArray = ["Find Mike", "Buy Eggs", "Destory Demogorgon"]
     
+    let defaults = UserDefaults.standard
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
     //MARK: TableView Datasource Methods
@@ -66,7 +72,10 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the Add Item Button on our UIAlert
             
+            
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
             self.tableView.reloadData()
         }
